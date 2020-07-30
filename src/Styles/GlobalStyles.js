06924f337876e822148a897d87a87002f869a2fc
zereight <zereight@zereightui-MacBook.local>
2020-07-30 15:24:38 +0900

    globalStyle and Theme

---
+++ src/Styles/GlobalStyles.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+export default createGlobalStyle`
+    ${reset};
+    * {
+        box-sizing: border-box;
+    }
+`;
