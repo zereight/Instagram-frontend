4adbf485f738f92d8ce0df3f56616b67901e1549
zereight <zereight@zereightui-MacBook.local>
2020-07-30 15:30:53 +0900

    error fixed
    App import path error

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,8 +3,8 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 
-export default () => {
+export default () => (
   <ThemeProvider theme={Theme}>
     <GlobalStyles />
-  </ThemeProvider>;
-};
+  </ThemeProvider>
+);
