6834547ac12861fbeaeb9afe0d0e4300a9de2daf
zereight <zereight@zereightui-MacBookPro.local>
2020-08-21 22:38:04 +0900

    React Router

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import { ThemeProvider } from "styled-components";
 import Theme from "../Styles/Theme";
 import GlobalStyles from "../Styles/GlobalStyles";
-
+import Router from "./Router";
 class App extends Component {
   render() {
     return (
       <ThemeProvider theme={Theme}>
-        <GlobalStyles />
+        <>
+          <GlobalStyles />
+          <Router isLoggedIn={false} />
+        </>
       </ThemeProvider>
     );
   }
---
+++ src/Components/Router.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../Routes/Auth";
+import Feed from "../Routes/Feed";
+
+const LoggedInRoutes = () => {
+  return (
+    <>
+      <Route exact path="/" component={Feed} />
+    </>
+  );
+};
+
+const LoggedOutRoutes = () => {
+  return (
+    <>
+      <Route exact path="/" component={Auth} />
+    </>
+  );
+};
+
+const AppRouter = ({ isLoggedIn }) => {
+  return (
+    <Router>
+      <Switch>{isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />}</Switch>
+    </Router>
+  );
+};
+
+AppRouter.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+};
+
+export default AppRouter;
