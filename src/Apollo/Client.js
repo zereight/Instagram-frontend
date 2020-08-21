7b0920a07fcbda678a4886c2dc36e881271a0455
zereight <zereight@zereightui-MacBookPro.local>
2020-08-21 22:20:56 +0900

    reset

--- src/Apollo/Client.js
+++
@@ -1,9 +0,0 @@
-import ApolloClient from "apollo-boost";
-import { defaults, resolvers } from "./LocalState";
-export default new ApolloClient({
-  uri: "http://localhost:4000",
-  clientState: {
-    defaults,
-    resolvers,
-  },
-});
