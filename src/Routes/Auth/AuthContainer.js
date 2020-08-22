3298e153e191b9387a359d91e3f074ceff209f2c
zereight <zereight@zereightui-MacBookPro.local>
2020-08-22 23:51:52 +0900

    confirmSecret + Log in mutation

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 import AuthPresenter from "./AuthPresenter";
 import useInput from "../../Hooks/useInput";
 import { useMutation } from "react-apollo-hooks";
-import { LOG_IN, CREATE_ACCOUNT } from "./AuthQueries";
+import {
+  LOG_IN,
+  CREATE_ACCOUNT,
+  CONFIRM_SECRET,
+  LOCAL_LOG_IN,
+} from "./AuthQueries";
 import { toast } from "react-toastify";
 
 export default () => {
@@ -11,7 +16,7 @@ export default () => {
   const firstName = useInput("");
   const lastName = useInput("");
   const secret = useInput("");
-  const email = useInput("duaxka7@naver.com");
+  const email = useInput("");
   const requestSecretMutation = useMutation(LOG_IN, {
     variables: { email: email.value },
   });
@@ -24,6 +29,15 @@ export default () => {
       lastName: lastName.value,
     },
   });
+
+  const confirmSecretMutation = useMutation(CONFIRM_SECRET, {
+    variables: {
+      email: email.value,
+      secret: secret.value,
+    },
+  });
+
+  const localLogInMutation = useMutation(LOCAL_LOG_IN);
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -68,6 +82,22 @@ export default () => {
         }
       } else {
         toast.error("All field are required");
+      }
+    } else if (action === "confirm") {
+      if (secret.value !== "") {
+        try {
+          const {
+            data: { confirmSecret: token },
+          } = await confirmSecretMutation();
+
+          if (token !== "" && token !== undefined) {
+            localLogInMutation({ variables: { token } });
+          } else {
+            throw Error();
+          }
+        } catch (error) {
+          toast.error("Can't confirm secret, check again.");
+        }
       }
     }
   };
