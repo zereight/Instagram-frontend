11c03ecd3d87cda89938e4a6e312230fcd8c0f96
zereight <zereight@zereightui-MacBookPro.local>
2020-08-22 20:03:48 +0900

    Auth Route UI part one

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,3 +1,23 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  min-height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Box = styled.div`
+  ${(props) => props.theme.whiteBox}
+`;
+
 export default () => {
-  return "Auth";
+  const [action, setAction] = useState("logIn");
+
+  return (
+    <Wrapper>
+      {action === "logIn" ? <Box>Log In</Box> : <Box>Sign Up</Box>}
+    </Wrapper>
+  );
 };
