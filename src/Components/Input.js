3b6303e3aa3de004f34fea921859ee7a22d0d280
zereight <zereight@zereightui-MacBookPro.local>
2020-08-23 13:22:46 +0900

    Auth Form with Hooks

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -12,12 +12,30 @@ const Container = styled.input`
   padding: 0px 15px;
 `;
 
-const Input = ({ placeholder }) => {
-  return <Container placeholder={placeholder}></Container>;
+const Input = ({
+  placeholder,
+  required = true,
+  value,
+  onChange,
+  type = "text",
+}) => {
+  return (
+    <Container
+      placeholder={placeholder}
+      required={required}
+      value={value}
+      onChange={onChange}
+      type={type}
+    ></Container>
+  );
 };
 
 Input.propTypes = {
   placeholder: PropTypes.string.isRequired,
+  required: PropTypes.bool,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  type: PropTypes.string,
 };
 
 export default Input;
