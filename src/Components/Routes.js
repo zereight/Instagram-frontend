62a10b813feea53bc10d98018b226dda47f70b55
zereight <zereight@zereightui-MacBookPro.local>
2020-08-25 23:45:53 +0900

    Search Screen Queries

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 import Feed from "../Routes/Feed";
 import Auth from "../Routes/Auth/index";
 import Explore from "../Routes/Explore";
-import Search from "../Routes/Search";
+import Search from "../Routes/Search/index";
 import Profile from "../Routes/Profile";
 
 const LoggedInRoutes = () => {
