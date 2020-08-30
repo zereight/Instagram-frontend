2ab8e9867d768827c76773d0ad0c3c1281b166bc
zereight <zereight@zereightui-MacBookPro.local>
2020-08-30 21:18:58 +0900

    GlobalStyles and Theme

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from "../Styles/Theme";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
 
 export default App;
